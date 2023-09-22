ae313eabc3052cc90388d9b26e8306cfef6b4488
AnaArgote <60635292+AnaArgote@users.noreply.github.com>
2023-09-22 01:27:32 -0600

    cambio ip

--- Ansible/JS/selectTipos.js
+++ Ansible/JS/selectTipos.js
@@ -2,7 +2,7 @@ var tabla = document.getElementById('tblTipos');
 var entradaTipo = document.getElementById('tipo');
 function Leer() {
     var peticion = new XMLHttpRequest(); //Hacer petición a backend
-    peticion.open('GET', 'http://192.168.1.74/desplegando/Ansible/PHP/LeerTipo.php');
+    peticion.open('GET', 'http://192.168.1.75/desplegando/Ansible/PHP/LeerTipo.php');
     peticion.send(); //Enviar petición
     peticion.onload = function () {
         console.log(peticion.responseText);
