9e72d7154d7a68fe711124ccb19e96444adbd5fa
AnaArgote <60635292+AnaArgote@users.noreply.github.com>
2023-09-21 23:30:47 -0600

    error

--- Ansible/JS/selectTipos.js
+++ Ansible/JS/selectTipos.js
@@ -1,6 +1,6 @@
 var tabla = document.getElementById('tblTipos');
 var entradaTipo = document.getElementById('tipo');
-function codigoFuncion() {
+function Leer() {
     var peticion = new XMLHttpRequest(); //Hacer petición a backend
     peticion.open('GET', 'http://192.168.1.74/desplegando/Ansible/PHP/LeerTipo.php');
     peticion.send(); //Enviar petición
@@ -21,5 +21,5 @@ function codigoFuncion() {
     }
 }
 window.onload = function(){
-    codigoFuncion();
+    Leer();
 }
